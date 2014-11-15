df35fcf1977e26efa6e0678664e1fe1c62ccb400
rgrannell1 <r.grannell2@gmail.com>
2014-11-15 23:10:19 +0000

    updated index.

---
+++ src/javascript/tmp-jquery.js
@@ -0,0 +1,15 @@
+
+var signinEvent = function () {
+
+	var credentials = {
+		email:    $("#email").val()
+		password: $("#password").val()
+	}
+
+	/*
+	request backend signin(credentials)
+	$.ajax({
+
+	})
+	*/
+}
