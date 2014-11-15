73f14d83247438e2c0ed4de844bcd9c5e8b0b6ba
rgrannell1 <r.grannell2@gmail.com>
2014-11-15 18:21:09 +0000

    added partially working validation code.

--- src/js/validate.js
+++ src/js/validate.js
@@ -16,7 +16,7 @@ var trimCredentials = function (user) {
 }
 
 var makeSalt = function () {
-	crypto.randomBytes(128).toString('base64')
+	return crypto.randomBytes(128).toString('base64')
 }
 
 // given a user object with a username and a password field,
@@ -24,8 +24,9 @@ var makeSalt = function () {
 
 var hashCredentials = function (user, salt, callback) {
 
-	//100,000 rounds is secure enough.
-	crypto.pbkdf2(user.password, salt, 100000, 128, function (err, derivedKey) {
+	const rounds = 100000    //100,000 rounds is secure enough.
+
+	crypto.pbkdf2(user.password, salt, rounds, 128, function (err, derivedKey) {
 
 		if (err) {
 			// don't print call stack.
@@ -46,8 +47,51 @@ var hashCredentials = function (user, salt, callback) {
 
 
 
+var isRegistered = function (user, callback) {
+	callback(true) // hard coded
+}
+
+var lookupUser = function (user, callback) {
+
+	callback({
+		user: 'bob',
+		password: '789232347924789234237894237894243mbh',
+		salt: '0'
+	})
+
+}
+
+
+
+
+
+// check if a user is registered, and compare their
+// hash against the hash in the database.
+
 var verifyLogin = function (user, callback) {
 
+	// is the user in the database?
+	isRegistered(user, function (isFound) {
+
+		if (!isFound) {
+			callback(false)
+		} else {
+
+			lookupUser(user, function (realCredentials) {
+
+				// check if the suppied credentials hash to the
+				// users actual stored hash.
+
+				// === is insecure way of comparing.
+
+				hashCredentials(user, realCredentials.salt, function (cred) {
+					callback(cred.password === realCredentials.password)
+				})
+
+			})
+
+		}
+	})
 }
 
 
@@ -57,7 +101,13 @@ var verifyLogin = function (user, callback) {
 
 
 
-hashCredentials({
-	username: "asdasdasd",
-	password: "asdasdasd"
-}, makeSalt(), console.log)
+var user = {
+	username: "bob",
+	password: "password12345"
+}
+
+
+
+
+
+verifyLogin(user, makeSalt, console.log)
